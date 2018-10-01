90a83a2f8b64bdf59c61fee4448e4ee43bcd4196
Yevhen Fisun <yevhen_fisun@epam.com>
2018-10-01 16:22:15 +0300

    hubUrl fix

--- src/test/java/com/epam/petclinic/BaseTest.java
+++ src/test/java/com/epam/petclinic/BaseTest.java
@@ -1,29 +1,28 @@
 package com.epam.petclinic;
 
 import java.net.MalformedURLException;
-import java.net.URI;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
 
     WebDriver driver;
 
-    @BeforeClass
-    public void setUp() {
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
         capabilities.setVersion("latest");
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", false);
 
-        try {
-            driver = new RemoteWebDriver(URI.create("http://127.0.0.1:4444/wd/hub").toURL(), capabilities);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        URL hubUrl = new URL("http://172.28.128.3:4444/wd/hub/");
+
+        driver = new RemoteWebDriver(hubUrl, capabilities);
+
     }
 }
