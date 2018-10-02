171e0377fa8de8ff8f8e1f19083872280ae6bb1a
Yevhen Fisun <yevhen_fisun@epam.com>
2018-10-02 13:06:24 +0300

    new try

--- src/test/java/com/epam/petclinic/PetclinicTests.java
+++ src/test/java/com/epam/petclinic/PetclinicTests.java
@@ -2,13 +2,27 @@ package com.epam.petclinic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class PetclinicTests extends BaseTest {
+public class PetclinicTests {
 
     @Test
-    public void test1() {
+    public void test1() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("latest");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        URL hubUrl = new URL("http://localhost:4444/wd/hub/");
+
+       RemoteWebDriver driver = new RemoteWebDriver(hubUrl, capabilities);
         driver.get("http://localhost:8888/");
         driver.findElement(By.cssSelector("[href='\\/vets\\.html']")).click();
         assertThat(driver.findElement(By.cssSelector("#vets")).isDisplayed()).isTrue();
